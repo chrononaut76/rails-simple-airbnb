6f65885fcf093c33c7c78a1ebb197f8323e29877
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-13 14:53:18 -0500

    Initial project setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+4.times do
+  new_flat = Flat.create!(
+    name: Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 2),
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentences(number: 4).join(' '),
+    price_per_night: (75..150).step(5).to_a.sample,
+    number_of_guests: (1..5).to_a.sample
+  )
+end
